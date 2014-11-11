b323d0d79da168dee07c5810de41305e27a7f24b
Dave Boyle <dboyle@criticalmass.com>
2014-11-11 11:01:17 -0500

    Created src directory, added more tasks to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,72 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
+    // configure the tasks
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
+        copy: {
+            build: {
+                cwd: "src",
+                src: ["**"],
+                dest: "app",
+                expand: true
+            }
+        },
+        clean: {
+            build: {
+                src: ["app"]
+            },
+            scripts: {
+                src: ["app/js/*.js", "!app/js/main.min.js"]
+                //You can add multiple ignore files
+                //"build/*.js", "!build/NodeMaker-min.js", "!build/Files.js"
+            }
+        },
+        uglify: {
+            build: {
+                options: {
+                    mangle: true
+                },
+                files: {
+                    "app/js/main.min.js": ["app/js/main.js"]
+                }
+            }
+        },
         watch: {
             livereload: {
                 // Here we watch the files the sass task will compile to
                 // These files are sent to the live reload server after sass compiles to them
                 options: { livereload: true },
                 files: ['app/**/*']
+            },
+            build: {
+                files: ["src/**"],
+                tasks: ["build"]
+            },
+            specs: {
+                files: ["spec/**"],
+                tasks: ["jasmine"]
             }
         },
+        connect: {
+            server: {
+                options: {
+                    port: 4000,
+                    base: "app",
+                    hostname: "*"
+                }
+            }
+        },
+        jasmine: {
+            pivotal: {
+                src: "path-to-file",
+                options: {
+                    specs: "path-to-specs",
+                    outfile: "SpecRunner.html",
+                    keepRunner: true
+                }
+            }
+        },
+        ,
         notify: {
             reload: {
                 options: {
@@ -25,22 +81,44 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'app/css/main.css': 'app/css/main.scss'
+                    'app/css/main.css': 'src/css/main.scss'
                 }
             }
         }
     });
 
-    grunt.event.on('watch', function(action, filepath, target) {
+    //EVENTS
+     grunt.event.on('watch', function(action, filepath, target) {
         grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
     });
 
-    // Load the plugin that provides the "watch" task.
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-notify');
+    //LOAD TASKS
+    //Copies files from source folder to build folder - command: grunt copy
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    //Wipes the build folder clean of files - command: grunt clean
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    //Minifies files - command: grunt uglify
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    //Watch files for changes - command: grunt watch
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    //Development server - command: grunt connect
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    //Unit testing framework
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    //Sass compiler
     grunt.loadNpmTasks('grunt-contrib-sass');
+    //Desktop notifier
+    grunt.loadNpmTasks('grunt-notify');
 
-    // Default task(s).
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('compile-sass', ['sass']);
+    //REGISTER TASKS
+    //Jasmine
+    grunt.registerTask("test", ["jasmine"]);
+    //Scripts
+    grunt.registerTask("scripts", "Uglifies and copies the Javascript files.", ["uglify", "clean:scripts"]);
+    //Build
+    grunt.registerTask("build-and-test", "Compiles all of the assets and copies the files to the app directory.", ["clean:build", "copy", "scripts", "jasmine"]);
+    //Build and Test
+    grunt.registerTask("build", "Compiles all of the assets and copies the files to the app directory.", ["clean:build", "copy", "scripts"]);
+    //Default - command: grunt default
+    grunt.registerTask("default", "Watches the project for changes, automatically builds them and runs a server.", ["build", "connect", "watch"]);
 };

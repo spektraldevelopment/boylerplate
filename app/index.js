c66279009df2ba7fd90666b83dcdb4c78d111d0d
Dave Boyle <dboyle@criticalmass.com>
2016-03-07 16:49:24 -0500

    [gulp] - Working on conditional statement for installing bootstrap-sass using bower.

--- app/index.js
+++ app/index.js
@@ -28,12 +28,18 @@ var BoylerplateGenerator = yeoman.generators.Base.extend({
         {
             name: 'gitUrl',
             message: 'Enter the git url: '
+        },{
+            type: 'confirm',
+            name: 'useBootstrap',
+            message: 'Would you like to use bootstrap-sass?',
+            default: true
         }];
 
         this.prompt(prompts, function (props) {
             this.appName = props.appName;
             this.desc = props.desc;
             this.gitUrl = props.gitUrl;
+            this.useBootstrap = props.useBootstrap;
             done();
         }.bind(this));
     },
@@ -48,23 +54,20 @@ var BoylerplateGenerator = yeoman.generators.Base.extend({
 
     copyFiles: function() {
 
-        var packageContext = {
+        var context = {
             app_name: this.appName,
             desc: this.desc,
-            gitUrl: this.gitUrl
-        };
-
-        var htmlContext = {
-            app_name: this.appName
+            gitUrl: this.gitUrl,
+            useBootstrap: this.useBootstrap
         };
 
         //Root files
         this.copy("gitignore", ".gitignore");
-        this.template("_package.json", "package.json", packageContext);
-        this.template("_bower.json", "bower.json", packageContext);
+        this.template("_package.json", "package.json", context);
+        this.template("_bower.json", "bower.json", context);
         this.template("_bowerrc", ".bowerrc");
         this.template("_gulpfile.js", "gulpfile.js");
-        this.template("_README.md", "README.md", htmlContext);
+        this.template("_README.md", "README.md", context);
 
         //SRC files
         //JS
@@ -74,7 +77,7 @@ var BoylerplateGenerator = yeoman.generators.Base.extend({
         this.copy("_config.scss", "src/sass/config.scss");
         this.copy("_main.scss", "src/sass/main.scss");
         //HTML
-        this.template("_index.html", "src/index.html", htmlContext);
+        this.template("_index.html", "src/index.html", context);
     },
 
     installDependents: function(){
